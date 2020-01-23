e4ae7d0f852e63bc45c9292c547495850e84930f
Percival N <percival.ngan@gmail.com>
2020-01-23 11:12:55 -0600

    Added starter project files

---
+++ iOS11-Pointers/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  iOS11-Pointers
+//
+//  Created by Percy Ngan on 1/23/20.
+//  Copyright © 2020 Lamdba School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+	return 0;
+}
