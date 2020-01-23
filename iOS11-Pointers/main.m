30f330154377acf082281a725b6ed6d1f06b6783
Percy <percival.ngan@gmail.com>
2020-01-23 12:27:28 -0600

    Added pointer codes

--- iOS11-Pointers/main.m
+++ iOS11-Pointers/main.m
@@ -8,6 +8,35 @@
 
 #import <Foundation/Foundation.h>
 
+// Goal: Print Your Name
+void pointerTest(void) {
+    char *message = "Hello xxxxxxxxxxxxxxxxxxxx!"; // '\0' = null terminated
+
+    printf("message pointer: %p\n", message);
+    printf("message: %s\n", message);
+
+    unsigned long length = strlen(message);
+    printf("Please allocate a string of size: %zu\n", sizeof(char) * length + 1);
+    char *ptr = malloc(sizeof(char) * length + 1);	// memory allocation
+    ptr = strcpy(ptr, message);
+
+    // Set your name!
+
+    ptr[6] = 'P'; // START HERE
+	ptr[7] = 'E';
+	ptr[8] = 'R';
+	ptr[9] = '\0';
+
+
+    printf("message: %s\n", ptr);
+
+
+    // Expected: message: Hello Paul!    // No x's!
+
+
+    free(ptr);
+}
+
 int main(int argc, const char * argv[]) {
 	@autoreleasepool {
 
@@ -20,13 +49,18 @@ int main(int argc, const char * argv[]) {
 		printf("count: %i\n", count);
 
 		// floating point numbers
-		float float_pi = 3.1459265359;
-		double pi = 3.1459265359;
+		float float_pi = 3.1459265359; // rounding to 3.14592648
+		double pi = 3.1459265359;	// more precise, but both are not exact (Don't use for currency)
 
 		printf("float_pi: %f\n", float_pi);
 		printf("pi: %f\n", pi);
 
+		int value = 42;
+		printf("value: %i\n", value);
 
+		char *ptr = "Hello iOS11!"; // value at ptr 
+		printf("ptr: %s\n", ptr);
+		printf("&ptr: %p\n", &ptr); // address of pointer
 
 
 	}
