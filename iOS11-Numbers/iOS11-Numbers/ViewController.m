03fb8f6c34b2d7ff0f95e04d87463ffd974fd8ea
Percy <percival.ngan@gmail.com>
2020-01-23 12:51:29 -0600

    Code for NSNumber and arrays

--- iOS11-Numbers/iOS11-Numbers/ViewController.m
+++ iOS11-Numbers/iOS11-Numbers/ViewController.m
@@ -16,8 +16,52 @@
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
-	// Do any additional setup after loading the view.
-}
+
+	// NsNumber
+
+//	NSArray *array = [[NSArray alloc] initWithObjects:1, 2, 3, nil]; // Error: Implicit conversion of 'int' to 'id _Nonnull' is disallowed with ARC
+// Objc: Arrays must hold objects not primitives (float, int, double, char)
+
+	// c-string literal = "abc"
+	// Objc String Literal = @"ABC"
+	// Number Literal = @1
+	// Array Literal = @[]
+
+	NSNumber *one = @1; //[NSNumber numberWithInt:1];
+	NSNumber *two = @2; //[NSNumber numberWithInt:2];
+	NSNumber *three = @3; //[NSNumber numberWithInt:3];
+
+	//	NSArray *array = [[NSArray alloc] initWithObjects:one, two, three, nil];
+
+	//	NSArray *array = [[NSArray alloc] initWithObjects:one, two, three, nil];
+
+	// Swift: [one, two, three]
+	//NSArray *array = @[one, two, three];
+
+	NSArray *array = @[@1, @2, @3];
+
+		double sum = [self calculateSum:@[@3.14, @400, @-45, @1000.1]];
+		printf("sum: %f\n", sum);
+
+
+		// Mutabilty
+
+	}
+
+	- (double)calculateSum:(NSArray *)array {
+		// TODO: Calculate the sum of the numbers in array
+
+		// doubleValue
+		double total = 0;
+
+		for (NSNumber *number in array) {
+//			total + number.doubleValue;
+			total += number.doubleValue;
+		}
+
+		return total;
+
+	}
 
 
 @end
